d40fb80f01b7f8a2d283736e4206db568ee3e5ee
danilivanyuk <danilivanyuk00@gmail.com>
2024-02-11 23:28:57 +0300

    added shadcn ui for start page

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,28 +1,36 @@
 
 <script lang="ts">
     import { goto } from "$app/navigation";
-    import { onDestroy } from "svelte";
-
     import { userNickname } from '../store'
+    import Button from "$lib/components/ui/button/button.svelte";
+    import * as Card from "$lib/components/ui/card";
+    import { Input } from "$lib/components/ui/input";
+    import { Label } from "$lib/components/ui/label";
     let nickname = '' 
 
     async function getUserData() {
 	    userNickname.set(nickname)
-	    // const res = await fetch('', {
-	    // 	method: 'POST',
-	    // 	body: JSON.stringify(nickname)
-	    // })
-	    // const response = res.json()
-	    // if (res.status === 200) {
-	    // 	goto('/user')
-     //    	}
-     //    	else {
-	    //     console.log('error', res)
-     //    	}
 	    goto('/user')
     }
 </script>
-
-<h1>LastFmStats</h1>
-<input bind:value={nickname} placeholder="Nickname" type='text'/>
-<button type="button" on:click={getUserData}>Confirm</button>
+<div class="grid h-screen place-items-center">
+  <Card.Root class="w-[350px] items-center">
+    <Card.Header>
+      <Card.Title>LastFmStats</Card.Title>
+      <Card.Description>Deep dive into yout lastfm stat</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <form>
+        <div class="grid w-full items-center gap-4">
+          <div class="flex flex-col space-y-1.5">
+            <Label for="name">Enter your lastfm nickname</Label>
+            <Input bind:value={nickname} placeholder="Nickname" type='text'/>
+          </div>
+        </div>
+      </form>
+    </Card.Content>
+    <Card.Footer class="flex justify-center">
+      <Button type="button" on:click={getUserData} placeholder={"Confirm"}>Confirm</Button>
+    </Card.Footer>
+  </Card.Root>
+</div>
