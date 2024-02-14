c4027ce2771f4e0ff335f0e39f85833c7aa8eb34
danilivanyuk <danilivanyuk00@gmail.com>
2024-02-15 00:04:01 +0300

    just list of artist

--- front/src/routes/user/+page.svelte
+++ front/src/routes/user/+page.svelte
@@ -3,10 +3,17 @@
     import { mockedData as data } from "../../../static/mockData"
     let nickname = $userNickname
     // TODO: if there is no nickname return back to "/"
+    const uniqueArtist = data.recenttracks.track.reduce((accumulator, currentValue) => (
+    accumulator.includes(currentValue.artist["#text"]) ? accumulator : [...accumulator, currentValue.artist["#text"]]
+  ), [] as {})
+    console.log(uniqueArtist)
 </script>
 <div class="w-[80%] mx-auto">
     <h1>Hello {nickname}</h1>
+    <!-- {uniqueArtist} -->
+    {#each Object.entries(uniqueArtist) as [index, values]}
+        <p>Artist: {values}</p>
 
-    <p>Top track: {data.recenttracks.track}</p>
+    {/each}
 
 </div>
