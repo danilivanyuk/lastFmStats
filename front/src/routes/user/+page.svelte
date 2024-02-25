033f05fbcccbfc21a00f1da2e7d93bdd3fa2ff48
danilivanyuk <danilivanyuk00@gmail.com>
2024-02-25 22:22:04 +0300

    table, scrobbles count

--- front/src/routes/user/+page.svelte
+++ front/src/routes/user/+page.svelte
@@ -3,13 +3,14 @@
     import type { TrackType } from "../../core/types/trackTypes";
     import type { IScrobblesDataSet } from "../../core/types/dataSetTypes";
     import { mockedData as globalData } from "../../../static/mockData"
+    import * as Table from "$lib/components/ui/table";
     let nickname = $userNickname
     const data = globalData.recenttracks.track[0] as unknown as TrackType
     let scrobbles: IScrobblesDataSet[] = []
+    let uniqueArtist: string[] = []
     // TODO: if there is no nickname return back to "/"
     const addArtist = () => {
         for (const [key, value] of Object.entries(globalData.recenttracks.track)) {
-            console.log(key, value)
             scrobbles.push({
                 track: value.name,
                 artist: value.artist["#text"],
@@ -17,26 +18,46 @@
                 date: parseInt(value.date?.uts || '0') ?? 0
             })
         }
-        console.log(scrobbles)
+        uniqueArtist = Array.from(new Set(scrobbles.map((item: IScrobblesDataSet) => item.artist)))
         scrobbles = scrobbles
-        // let artistData: IArtistDataSet = Object.entries(data as unknown as TrackType).reduce()
+    }
+    const getArtistScrobbles = (artist: string) => {
+        const test = scrobbles.filter(function(scrobble: IScrobblesDataSet) {
+        return (scrobble.artist === artist)
+        }).length
+        return test
+    }
+    const getUniqueTracks = (artist: string) => {
+
     }
 </script>
 <div class="w-[80%] mx-auto">
     <h1>Hello {nickname}</h1>
     <button on:click={addArtist}>Load info</button>
-    <!-- {uniqueArtist} -->
-    {#each (scrobbles) as scrobble}
-        <div>
-<!-- <img src={data.recenttracks.track[index].image[0]["#text"]} alt=""> -->
-            <!-- {data.recenttracks.track[index].image[0]["#text"]} -->
-            <p>Artist: {scrobble.artist}</p>
-            <p>Track: {scrobble.track}</p>
-            <p>Album: {scrobble.album}</p>
-            <p>Date: {new Date((scrobble.date)).toISOString()}</p>
-            <p>------</p>
-        </div>
+    <Table.Root>
+      <Table.Caption>Dataset</Table.Caption>
+      <Table.Header>
+        <Table.Row>
+          <Table.Head class="w-[100px]">Artist</Table.Head>
+          <Table.Head>Tracks</Table.Head>
+          <Table.Head>Scrobbles</Table.Head>
+          <Table.Head class="text-right">Rank</Table.Head>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+                {#each (uniqueArtist) as artist}
 
-    {/each}
+        <Table.Row>
+          <Table.Cell class="font-medium">{artist}</Table.Cell>
+          <Table.Cell>{getUniqueTracks(artist)}</Table.Cell>
+          <Table.Cell>{getArtistScrobbles(artist)}</Table.Cell>
+          <Table.Cell class="text-right">Rank</Table.Cell>
+        </Table.Row>
+                    {/each}
+
+      </Table.Body>
+    </Table.Root>
+
+
 
 </div>
